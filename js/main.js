db3815cde5b6cdc065221324905767bea41549bd
zhzhang <zhzhang@mygene.com>
2017-08-07 18:43:22 +0800

    使用vue， 将播放器部分独立出来

--- js/main.js
+++ js/main.js
@@ -1,33 +1,92 @@
 window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
-window.onload = function () {
-    var i = 0;
-    var list = [
-        './mp3/许镜清 - 天府乐.mp3',
-        './mp3/Dj Titon - 死机之歌.mp3',
-        './mp3/John Williams - Twentieth Century Fox Fanfare with CinemaScope Extension.mp3',
-        './mp3/许镜清 - 云宫迅音.mp3',
-        './mp3/MetroGnome - Ringtone (MetroGnome Remix).mp3',
-        './mp3/Space - Just Blue.mp3',
-        './mp3/胡伟立 - 市集.mp3',
-        './mp3/胡伟立 - 偷功.mp3',
-        './mp3/卢冠廷 - 賭神.mp3',
-        './mp3/群星 - 渔舟唱晚 (cut版).mp3',
-        './mp3/文武贝 - 新闻联播片尾曲-文武贝重新制作.mp3',
-        './mp3/武聆音雄 - 天龙八部间奏曲.mp3',
-        './mp3/中国人民解放军军乐团 - 运动员进行曲.mp3'
-    ];
-    var audio = document.getElementById('audio');
-    audio.setAttribute('src', list[i]);
-    audio.onended = function () {
-        i++;
-        if (list[i]) {
-            this.setAttribute('src', list[i]);
-        } else {
-            i = 0;
-            this.setAttribute('src', list[i]);
+var audio = document.getElementById('audio');
+var ctx = new AudioContext();
+var vm = new Vue({
+    el: '#app',
+    data: {
+        musicList: [
+            './mp3/许镜清 - 天府乐.mp3',
+            './mp3/Dj Titon - 死机之歌.mp3',
+            './mp3/John Williams - Twentieth Century Fox Fanfare with CinemaScope Extension.mp3',
+            './mp3/许镜清 - 云宫迅音.mp3',
+            './mp3/MetroGnome - Ringtone (MetroGnome Remix).mp3',
+            './mp3/Space - Just Blue.mp3',
+            './mp3/胡伟立 - 市集.mp3',
+            './mp3/胡伟立 - 偷功.mp3',
+            './mp3/卢冠廷 - 賭神.mp3',
+            './mp3/群星 - 渔舟唱晚 (cut版).mp3',
+            './mp3/文武贝 - 新闻联播片尾曲-文武贝重新制作.mp3',
+            './mp3/武聆音雄 - 天龙八部间奏曲.mp3',
+            './mp3/中国人民解放军军乐团 - 运动员进行曲.mp3'
+        ],
+        currentMusic: 0,
+        isPlay: 0,
+        showList: 0
+    },
+    methods: {
+        setPlay: function (index) {
+            var musicList = this.musicList
+            this.currentMusic = index
+            if (index < 0 || index > musicList.length) {
+                console.log('没有该歌曲!');
+            }
+            audio.setAttribute('src', musicList[index]);
+        },
+        play: function () {
+            audio.play();
+            this.isPlay = 1;
+        },
+        pause: function () {
+            audio.pause();
+            this.isPlay = 0;
+        },
+        playAndPause: function () {
+            this.isPlay = !this.isPlay;
+            if (this.isPlay) {
+                this.play();
+            } else {
+                this.pause();
+            }
+        },
+        next: function () {
+          this.handleEnded();
+        },
+        prev: function () {
+            if (this.currentMusic === 0) {
+                this.currentMusic = this.musicList.length - 1;
+            } else {
+                this.currentMusic--;
+            }
+        },
+        handleEnded: function () {
+            if (this.currentMusic === this.musicList.length - 1) {
+                this.currentMusic = 0;
+            } else {
+                this.currentMusic++;
+            }
+        },
+        clickMusic: function (index) {
+            this.currentMusic = index;
         }
-    };
-    var ctx = new AudioContext();
+    },
+    watch:{
+        'currentMusic': function (index) {
+            this.setPlay(index);
+            this.play();
+        }
+    },
+    filters: {
+        getMp3Name: function (value) {
+            return value.replace(/.\/mp3\//, '')
+        }
+    },
+    created: function () {
+        console.log('mounted')
+        this.setPlay(this.currentMusic)
+    }
+})
+
+window.onload = function () {
     var analyser = ctx.createAnalyser();
     var audioSrc = ctx.createMediaElementSource(audio);
     audioSrc.connect(analyser);
@@ -67,32 +126,5 @@ window.onload = function () {
         }
         requestAnimationFrame(renderFrame);
     }
-
     renderFrame();
-    var parse = document.getElementById('parse');
-    parse.addEventListener('click', function () {
-        if (this.getAttribute('class') === 'parse') {
-            audio.pause();
-            parse.setAttribute('class', 'play')
-        } else {
-            audio.play();
-            parse.setAttribute('class', 'parse')
-        }
-    });
-
-    var prev = document.getElementById('prev');
-    prev.addEventListener('click', function () {
-        i--;
-        if (list[i]) {
-            audio.setAttribute('src', list[i]);
-        } else {
-            i = list.length - 1;
-            audio.setAttribute('src', list[i]);
-        }
-    });
-
-    var next = document.getElementById('next');
-    next.addEventListener('click', function () {
-        audio.onended()
-    })
 };
